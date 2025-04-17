78452da464d291533df33d616ebd7d04d68e593b
Nicole Kiunuhe <167426106+nicolemk12@users.noreply.github.com>
2025-04-17 17:49:17 +0200

    Upload

---
+++ src/bv_ss25/RasterImage.java
@@ -0,0 +1,103 @@
+// BV Ue1 SS2025 Vorgabe
+//
+// Copyright (C) 2025 by Klaus Jung
+// All rights reserved.
+// Date: 2025-04-02
+ 		   	  	 	 	
+
+package bv_ss25;
+
+import java.io.File;
+import java.util.Arrays;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
+public class RasterImage {
+ 		   	  	 	 	
+	private static final int gray  = 0xffa0a0a0;
+
+	public int[] argb;	// pixels represented as ARGB values in scanline order
+	public int width;	// image width in pixels
+	public int height;	// image height in pixels
+	
+	public RasterImage(int width, int height) {
+		// creates an empty RasterImage of given size
+		this.width = width;
+		this.height = height;
+		argb = new int[width * height];
+		Arrays.fill(argb, gray);
+	}
+	
+	public RasterImage(RasterImage src) {
+		// copy constructor
+		this.width = src.width;
+		this.height = src.height;
+		argb = src.argb.clone();
+	}
+	
+	public RasterImage(File file) {
+		// creates an RasterImage by reading the given file
+		Image image = null;
+		if(file != null && file.exists()) {
+			image = new Image(file.toURI().toString());
+		}
+		if(image != null && image.getPixelReader() != null) {
+			width = (int)image.getWidth();
+			height = (int)image.getHeight();
+			argb = new int[width * height];
+			image.getPixelReader().getPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), argb, 0, width);
+		} else {
+			// file reading failed: create an empty RasterImage
+			this.width = 256;
+			this.height = 256;
+			argb = new int[width * height];
+			Arrays.fill(argb, gray);
+		}
+	}
+	
+	public RasterImage(ImageView imageView) {
+		// creates a RasterImage from that what is shown in the given ImageView
+		Image image = imageView.getImage();
+		width = (int)image.getWidth();
+		height = (int)image.getHeight();
+		argb = new int[width * height];
+		image.getPixelReader().getPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), argb, 0, width);
+	}
+	
+	public void setToView(ImageView imageView) {
+		// sets the current argb pixels to be shown in the given ImageView
+		if(argb != null) {
+			WritableImage wr = new WritableImage(width, height);
+			PixelWriter pw = wr.getPixelWriter();
+			pw.setPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), argb, 0, width);
+			imageView.setImage(wr);
+		}
+	}
+		
+	// image point operations to be added here
+	
+	public void convertToGray() {
+		// TODO: convert the image to grayscale
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				int pos = y * width + x;
+				int argbColor = argb[pos];
+				int r = (argbColor >> 16) & 0xff;
+				int g = (argbColor >> 8) & 0xff;
+				int b = argbColor & 0xff;
+				int gray = (r + g + b) / 3;
+				// Combine alpha channel with gray value (RGB all equal)
+				argb[pos] = (argbColor & 0xff000000) | (gray << 16) | (gray << 8) | gray;
+			}
+		}
+	}
+	
+ 		   	  	 	 	
+
+}
+ 		   	  	 	 	
+
